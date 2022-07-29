a1ea70e477af2fc05b7fef9c831d5785d8530a95
jubomi <bomi050623@gmail.com>
2022-07-29 14:27:45 +0900

    [UPDATE]7/29

--- memoo/memoo/MemoListTableViewController.swift
+++ memoo/memoo/MemoListTableViewController.swift
@@ -8,9 +8,30 @@
 import UIKit
 
 class MemoListTableViewController: UITableViewController {
-
+    let formatter: DateFormatter = {
+        let f = DateFormatter()
+        f.dateStyle = .long
+        f.timeStyle = .short
+        f.locale = Locale(identifier: "Ko_kr")
+        return f
+    }()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    var token: NSObjectProtocol?
+    
+    deinit {
+        if let token = token {
+            NotificationCenter.default.removeObserver(token)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+       token = NotificationCenter.default.addObserver(forName: ComposeViewController.newMemoDidInsert, object: nil, queue: OperationQueue.main) {[weak self] (noti) in self?.tableView.reloadData()}
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -23,18 +44,17 @@ class MemoListTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return Memo.dummyMemoList.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let target = Memo.dummyMemoList[indexPath.row]
+        cell.textLabel?.text = target.content
+        cell.detailTextLabel?.text = formatter.string(from: target.insertDate)
+        
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
---
+++ memoo/memoo/UIViewController+allert.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController+allert.swift
+//  memoo
+//
+//  Created by 주보미 on 2022/07/28.
+//
+
+import UIKit
+
+extension UIViewController {
+    func alert(title: String = "알림", message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+        
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+}
